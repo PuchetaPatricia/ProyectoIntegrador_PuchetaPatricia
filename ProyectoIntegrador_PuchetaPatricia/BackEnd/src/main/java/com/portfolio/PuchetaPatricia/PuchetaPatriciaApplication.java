e063d46f70564e827ac6006d017feedf0dc68672
patricia pucheta <ptrcpucheta@gmail.com>
2022-08-04 22:16:37 -0300

    creacion de la carpeta backend con la inicializacion del proyecto

---
+++ ProyectoIntegrador_PuchetaPatricia/BackEnd/src/main/java/com/portfolio/PuchetaPatricia/PuchetaPatriciaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.PuchetaPatricia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuchetaPatriciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuchetaPatriciaApplication.class, args);
+	}
+
+}
---
+++ ProyectoIntegrador_PuchetaPatricia/BackEnd/src/test/java/com/portfolio/PuchetaPatricia/PuchetaPatriciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.PuchetaPatricia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuchetaPatriciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
